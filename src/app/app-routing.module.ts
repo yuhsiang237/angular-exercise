18f03a5670858263752965a09238ab619055fc50
yu hsiang <lin@test.com>
2025-06-28 09:05:00 +0800

    angular基礎操作

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { BasicComponent } from './pages/basic/basic.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'basic', component: BasicComponent },
   // 萬用路由（未知路徑轉向 Home）
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { BasicComponent } from './pages/basic/basic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    BasicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    /**
+     * 如果想在 Angular 中使用 雙向資料綁定 [(ngModel)]，就一定要先引入 FormsModule。
+      ngModel 是屬於 Template-driven Form 的功能，Angular 不會自動幫你加入這個模組，你要手動在 AppModule 中引入。
+     */
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
